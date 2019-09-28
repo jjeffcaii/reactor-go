406876802a868aeb1c633c75cbfe2759171001fc
Jeffsky <jjeffcaii@outlook.com>
2019-09-28 20:35:05 +0800

    fix compatibility with ants.

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/jjeffcaii/reactor-go
 go 1.12
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/panjf2000/ants v1.0.0
-	github.com/stretchr/testify v1.3.0
+	github.com/panjf2000/ants v1.2.0
+	github.com/stretchr/testify v1.4.0
 )
--- scheduler/elastic.go
+++ scheduler/elastic.go
@@ -1,45 +1,46 @@
 package scheduler
 
 import (
-	"math"
+  "math"
 
-	"github.com/panjf2000/ants"
+  "github.com/panjf2000/ants"
 )
 
 var elastic Scheduler
 
 func init() {
-	elastic = NewElastic(math.MaxInt32)
+  elastic = NewElastic(math.MaxInt32)
 }
 
 type elasticScheduler struct {
-	pool *ants.Pool
+  pool *ants.Pool
 }
 
-func (p *elasticScheduler) Close() error {
-	return p.pool.Release()
+func (p *elasticScheduler) Close() (err error) {
+  p.pool.Release()
+  return
 }
 
 func (p *elasticScheduler) Do(job Job) {
-	if err := p.pool.Submit(job); err != nil {
-		panic(err)
-	}
+  if err := p.pool.Submit(job); err != nil {
+    panic(err)
+  }
 }
 
 func (p *elasticScheduler) Worker() Worker {
-	return p
+  return p
 }
 
 func NewElastic(size int) Scheduler {
-	pool, err := ants.NewPool(size)
-	if err != nil {
-		panic(err)
-	}
-	return &elasticScheduler{
-		pool: pool,
-	}
+  pool, err := ants.NewPool(size)
+  if err != nil {
+    panic(err)
+  }
+  return &elasticScheduler{
+    pool: pool,
+  }
 }
 
 func Elastic() Scheduler {
-	return elastic
+  return elastic
 }
