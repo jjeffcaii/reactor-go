6e5b5bbed9e52b0d1e12f4fc1b677ab4d08a19f1
king <yjqqqqdx_01@163.com>
2020-10-09 11:25:50 +0800

    Update wrapper.go
    Add object pool, sync.pool Reuse wrap

--- mono/wrapper.go
+++ mono/wrapper.go
@@ -3,12 +3,19 @@ package mono
 import (
 	"context"
 	"errors"
+	"sync"
 	"time"
 
 	"github.com/jjeffcaii/reactor-go"
 	"github.com/jjeffcaii/reactor-go/internal/subscribers"
 	"github.com/jjeffcaii/reactor-go/scheduler"
 )
+
+var pool *sync.Pool = &sync.Pool{
+	New: func() interface{} {
+		return wrap(nil)
+	},
+}
 
 var errNotProcessor = errors.New("publisher is not a Processor")
 
@@ -18,65 +25,95 @@ type wrapper struct {
 
 func (p wrapper) Subscribe(ctx context.Context, options ...reactor.SubscriberOption) {
 	p.SubscribeWith(ctx, reactor.NewSubscriber(options...))
+	putObjToPool(&p)
 }
 
 func (p wrapper) SwitchIfEmpty(alternative Mono) Mono {
-	return wrap(newMonoSwitchIfEmpty(p.RawPublisher, alternative))
+	var w = getObjFromPool(newMonoSwitchIfEmpty(p.RawPublisher, alternative))
+	putObjToPool(&p)
+	return w
 }
 
 func (p wrapper) Filter(f reactor.Predicate) Mono {
-	return wrap(newMonoFilter(p.RawPublisher, f))
+	var w = getObjFromPool(newMonoFilter(p.RawPublisher, f))
+	putObjToPool(&p)
+	return w
 }
 
 func (p wrapper) Map(t reactor.Transformer) Mono {
-	return wrap(newMonoMap(p.RawPublisher, t))
+	var w = getObjFromPool(newMonoMap(p.RawPublisher, t))
+	putObjToPool(&p)
+	return w
+
 }
 
 func (p wrapper) FlatMap(mapper FlatMapper) Mono {
-	return wrap(newMonoFlatMap(p.RawPublisher, mapper))
+	var w = getObjFromPool(newMonoFlatMap(p.RawPublisher, mapper))
+	putObjToPool(&p)
+	return w
 }
 
 func (p wrapper) SubscribeOn(sc scheduler.Scheduler) Mono {
-	return wrap(newMonoScheduleOn(p.RawPublisher, sc))
+	var w = getObjFromPool(newMonoScheduleOn(p.RawPublisher, sc))
+	putObjToPool(&p)
+	return w
 }
 
 func (p wrapper) DoOnNext(fn reactor.FnOnNext) Mono {
-	return wrap(newMonoPeek(p.RawPublisher, peekNext(fn)))
+	var w = getObjFromPool(newMonoPeek(p.RawPublisher, peekNext(fn)))
+	putObjToPool(&p)
+	return w
 }
 
 func (p wrapper) DoOnError(fn reactor.FnOnError) Mono {
-	return wrap(newMonoPeek(p.RawPublisher, peekError(fn)))
+	var w = getObjFromPool(newMonoPeek(p.RawPublisher, peekError(fn)))
+	putObjToPool(&p)
+	return w
 }
 
 func (p wrapper) DoOnComplete(fn reactor.FnOnComplete) Mono {
-	return wrap(newMonoPeek(p.RawPublisher, peekComplete(fn)))
+	var w = getObjFromPool(newMonoPeek(p.RawPublisher, peekComplete(fn)))
+	putObjToPool(&p)
+	return w
 }
 
 func (p wrapper) DoOnCancel(fn reactor.FnOnCancel) Mono {
-	return wrap(newMonoPeek(p.RawPublisher, peekCancel(fn)))
+	var w = getObjFromPool(newMonoPeek(p.RawPublisher, peekCancel(fn)))
+	putObjToPool(&p)
+	return w
 }
 
 func (p wrapper) DoOnDiscard(fn reactor.FnOnDiscard) Mono {
-	return wrap(newMonoContext(p.RawPublisher, withContextDiscard(fn)))
+	var w = getObjFromPool(newMonoContext(p.RawPublisher, withContextDiscard(fn)))
+	putObjToPool(&p)
+	return w
 }
 
 func (p wrapper) DoFinally(fn reactor.FnOnFinally) Mono {
-	return wrap(newMonoDoFinally(p.RawPublisher, fn))
+	var w = getObjFromPool(newMonoDoFinally(p.RawPublisher, fn))
+	putObjToPool(&p)
+	return w
 }
 
 func (p wrapper) DoOnSubscribe(fn reactor.FnOnSubscribe) Mono {
-	return wrap(newMonoPeek(p.RawPublisher, peekSubscribe(fn)))
+	var w = getObjFromPool(newMonoPeek(p.RawPublisher, peekSubscribe(fn)))
+	putObjToPool(&p)
+	return w
 }
 
 func (p wrapper) DelayElement(delay time.Duration) Mono {
-	return wrap(newMonoDelayElement(p.RawPublisher, delay, scheduler.Elastic()))
+	var w = getObjFromPool(newMonoDelayElement(p.RawPublisher, delay, scheduler.Elastic()))
+	putObjToPool(&p)
+	return w
 }
 
 func (p wrapper) Timeout(timeout time.Duration) Mono {
 	if timeout <= 0 {
 		return p
 	}
-	return wrap(newMonoTimeout(p.RawPublisher, timeout))
+	var w = getObjFromPool(newMonoTimeout(p.RawPublisher, timeout))
+	putObjToPool(&p)
+	return w
 }
 
 func (p wrapper) Block(ctx context.Context) (Any, error) {
@@ -89,7 +126,7 @@ func (p wrapper) Block(ctx context.Context) (Any, error) {
 
 	defer close(vchan)
 	defer close(echan)
-
+	putObjToPool(&p)
 	select {
 	case value := <-vchan:
 		return value, nil
@@ -102,10 +139,12 @@ func (p wrapper) Block(ctx context.Context) (Any, error) {
 
 func (p wrapper) Success(v Any) {
 	p.mustProcessor().Success(v)
+	putObjToPool(&p)
 }
 
 func (p wrapper) Error(e error) {
 	p.mustProcessor().Error(e)
+	putObjToPool(&p)
 }
 
 func (p wrapper) mustProcessor() *processor {
@@ -113,7 +152,19 @@ func (p wrapper) mustProcessor() *processor {
 	if !ok {
 		panic(errNotProcessor)
 	}
+	putObjToPool(&p)
 	return pp
+}
+
+func getObjFromPool(r reactor.RawPublisher) wrapper {
+	var w = pool.Get().(wrapper)
+	w.RawPublisher = r
+	return w
+}
+
+func putObjToPool(w *wrapper)  {
+	w.RawPublisher = nil
+	pool.Put(*w)
 }
 
 func wrap(r reactor.RawPublisher) wrapper {
