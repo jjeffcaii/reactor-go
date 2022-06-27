a7bd3e9cadea914a56092be7cfefe48d2167d6dc
Jeffsky <jjeffcaii@outlook.com>
2022-06-27 14:53:47 +0800

    fix: race problem in timeout mono (#42)
    * fix: race problem in timeout mono
    
    * add ut
    
    * fix ut
    
    * set codecov

--- mono/timeout_test.go
+++ mono/timeout_test.go
@@ -2,6 +2,7 @@ package mono
 
 import (
 	"context"
+	"io"
 	"testing"
 	"time"
 
@@ -32,4 +33,88 @@ func TestMonoTimeout_SubscribeWith(t *testing.T) {
 	s.EXPECT().OnComplete().Times(1)
 	newMonoTimeout(source, 20*time.Millisecond).SubscribeWith(context.Background(), s)
 	time.Sleep(30 * time.Millisecond)
+}
+
+func TestTimeoutSubscriber(t *testing.T) {
+	t.Run("ErrorCompleteNext", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		ms := NewMockSubscription(ctrl)
+
+		ms.EXPECT().Request(gomock.Any()).AnyTimes()
+		ms.EXPECT().Cancel().AnyTimes()
+
+		msub := NewMockSubscriber(ctrl)
+		msub.EXPECT().OnSubscribe(gomock.Any(), gomock.Any()).Do(MockRequestInfinite).Times(1)
+		msub.EXPECT().OnError(gomock.Any()).Times(1)
+		msub.EXPECT().OnNext(gomock.Any()).Times(0)
+		msub.EXPECT().OnComplete().Times(0)
+
+		sub := &timeoutSubscriber{
+			actual: msub,
+			done:   make(chan struct{}),
+		}
+
+		sub.OnSubscribe(context.Background(), ms)
+
+		sub.OnError(io.EOF)
+		sub.OnComplete()
+		sub.OnNext(1)
+	})
+
+	t.Run("CompleteErrorNext", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		ms := NewMockSubscription(ctrl)
+
+		ms.EXPECT().Request(gomock.Any()).AnyTimes()
+		ms.EXPECT().Cancel().AnyTimes()
+
+		msub := NewMockSubscriber(ctrl)
+		msub.EXPECT().OnSubscribe(gomock.Any(), gomock.Any()).Do(MockRequestInfinite).Times(1)
+		msub.EXPECT().OnError(gomock.Any()).Times(0)
+		msub.EXPECT().OnNext(gomock.Any()).Times(0)
+		msub.EXPECT().OnComplete().Times(1)
+
+		sub := &timeoutSubscriber{
+			actual: msub,
+			done:   make(chan struct{}),
+		}
+
+		sub.OnSubscribe(context.Background(), ms)
+
+		sub.OnComplete()
+		sub.OnError(io.EOF)
+		sub.OnNext(1)
+	})
+
+	t.Run("NextErrorComplete", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		ms := NewMockSubscription(ctrl)
+
+		ms.EXPECT().Request(gomock.Any()).AnyTimes()
+		ms.EXPECT().Cancel().AnyTimes()
+
+		msub := NewMockSubscriber(ctrl)
+		msub.EXPECT().OnSubscribe(gomock.Any(), gomock.Any()).Do(MockRequestInfinite).Times(1)
+		msub.EXPECT().OnError(gomock.Any()).Times(0)
+		msub.EXPECT().OnNext(gomock.Any()).Times(1)
+		msub.EXPECT().OnComplete().Times(1)
+
+		sub := &timeoutSubscriber{
+			actual: msub,
+			done:   make(chan struct{}),
+		}
+
+		sub.OnSubscribe(context.Background(), ms)
+
+		sub.OnNext(1)
+		sub.OnError(io.EOF)
+		sub.OnComplete()
+	})
+
 }
